2aacf507a3192c77ba7a47b5fc31e16a1c499a80
Rasheed-Bennett <bennettrasheed9@gmail.com>
2026-05-29 23:53:18 -0600

    controlTemporizador

--- src/App.jsx
+++ src/App.jsx
@@ -13,17 +13,33 @@ function App() {
   const [semaforoAmarillo, setSemaforoAmarillo] = useState(0)
   const tiempoAmarillo = useRef(0)
   const tiempoVerde = useRef(1)
-  const temporizadorAmarillo = useRef(3)
-  const temporizadorVerde = useRef(6)
+  const [temporizadorAmarillo, setTemporizadorAmarillo] = useState(3)
+  const [temporizadorVerde,setTemporizadorVerde] = useState(6)
   const [semaforo1, setSemaforo1] = useState(semaforoV)
   const [semaforo2, setSemaforo2] = useState(semaforoR)
   const [semaforo3, setSemaforo3] = useState(semaforoR)
   const [semaforo4, setSemaforo4] = useState(semaforoR)
+  const [valor, setValor] = useState(0)
+  const [nuevoTiempo, setNuevoTiempo] = useState(null)
+
+  const handleTiempoA = () => {
+    if (valor <= temporizadorVerde){
+      setTemporizadorAmarillo(valor)
+      setValor(0)
+    }
+  }
+
+  const handleTiempoV = () => {
+    if (valor >= temporizadorAmarillo){
+      setTemporizadorVerde(valor)
+      setValor(0)
+    }
+  }
 
   useEffect(() =>{
     const tiempo = setInterval (() => {
       tiempoAmarillo.current = tiempoAmarillo.current + 1;
-      if (tiempoAmarillo.current >= 3) {
+      if (tiempoAmarillo.current >= temporizadorAmarillo) {
         tiempoAmarillo.current = 0;
         setSemaforoAmarillo((prev) => {
           switch(prev){
@@ -51,7 +67,7 @@ function App() {
   useEffect(() =>{
     const temporizador = setInterval(() =>{
       tiempoVerde.current = tiempoVerde.current +1
-      if (tiempoVerde.current >= 6) {
+      if (tiempoVerde.current >= temporizadorVerde) {
         tiempoVerde.current = 0;
           switch(semaforoVerde){
             case 1:
@@ -146,6 +162,32 @@ function App() {
             transform: 'rotate(90deg)'
           }}
         />
+          <input
+            type="number"
+            value={valor}
+            onChange= {(e) => setValor(parseInt(e.target.value, 10))}
+            placeholder='Ingresa el nuevo tiempo'
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              gap: '8px',
+              alignItems: 'flex-start'
+            }} 
+          />
+          <button onClick={handleTiempoA}
+          style={{
+              display: 'flex',
+              flexDirection: 'column',
+              gap: '8px',
+              alignItems: 'flex-start'
+            }} >Cambiar tiempo amarillo</button>
+          <button onClick={handleTiempoV}
+          style={{
+              display: 'flex',
+              flexDirection: 'column',
+              gap: '8px',
+              alignItems: 'flex-start'
+            }} >Cambiar tiempo Verde</button>
       </div>
     </>
   )
